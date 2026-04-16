7fc8ce983cc28d4b352ace4907f66a823b4479ec
vhehajdajd <vhehajdajd@gmail.com>
2026-04-16 11:10:06 +0900

    공지 브랜치 변경

--- src/main/java/com/hometalk/onepass/notice/dto/NoticeDetailResponseDto.java
+++ src/main/java/com/hometalk/onepass/notice/dto/NoticeDetailResponseDto.java
@@ -1,32 +1,4 @@
 package com.hometalk.onepass.notice.dto;
 
-
-import com.hometalk.onepass.notice.entity.Badge;
-import lombok.*;
-
-import java.time.LocalDateTime;
-
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@ToString
 public class NoticeDetailResponseDto {
-
-    private Long id;
-
-    private String title;
-
-    private String content;
-
-    private int viewCount;
-
-    private Badge badge;
-
-    private Boolean isPinned;
-
-    private LocalDateTime createdAt;
-
-    private LocalDateTime updatedAt;
-
 }
